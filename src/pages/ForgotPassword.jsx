68077e9dbf54cbcae45b26e0cf37c414cdaafa6d
Nayana Singh <nayanak872@gmail.com>
2022-12-10 15:14:19 +0530

    add password reset functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,25 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import OAuth from '../components/OAuth';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   function onChange(e){
     setEmail(e.target.value);
+  }
+
+  async function onSubmit(e){
+e.preventDefault();
+try{
+const auth = getAuth()
+await sendPasswordResetEmail(auth, email)
+toast.success("Email was sent");
+}
+catch(error){
+toast.error("Could not send reset password")
+}
   }
   return (
     <section>
@@ -15,7 +29,7 @@ export default function ForgotPassword() {
 <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="key" className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-       <form>
+       <form onSubmit={onSubmit}>
   <input className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" type="email" id="email" value={email} onChange={onChange}placeholder="📧 Enter your email"/>
     <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
       <p className="mb-6">
