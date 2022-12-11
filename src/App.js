8bbfd88a43a6167dd550fd1b107543b79e96ef51
Nayana Singh <nayanak872@gmail.com>
2022-12-11 19:03:20 +0530

    create private route and custom hook forfor logging in and rendering profile only to authenticated users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
+import PrivateRoute from './components/PrivateRoute';
 import Offers from "./pages/Offers"
 import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
@@ -15,12 +16,13 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-        <Route path="/offers" element={<Offers />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<PrivateRoute />} >
+        <Route path="/profile" element={<Profile />} /></Route>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
       </Routes>
 </Router>
 <ToastContainer
