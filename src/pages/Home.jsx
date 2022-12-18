606afa9c0aed1ef03c90dc014ee6258954a01934
Nayana Singh <nayanak872@gmail.com>
2022-12-18 18:32:22 +0530

    create Slider component and add to the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Slider from '../components/Slider';
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Slider />
+    </div>
   )
 }
 
-export default Home
