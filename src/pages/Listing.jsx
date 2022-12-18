5b8209e4b3553303687ff2fdc55c805d2e88341d
Nayana Singh <nayanak872@gmail.com>
2022-12-18 15:33:02 +0530

    Create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,12 +12,16 @@ import SwiperCore, {
 import "swiper/css/bundle";
 import Spinner from '../components/Spinner';
 import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from 'react-icons/fa';
+import {getAuth} from "firebase/auth"
+import Contact from '../components/Contact';
 
 export default function Listing() {
+  const auth = getAuth();
   const params = useParams()
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination])
   useEffect(() => {
      async function fetchListing(){
@@ -69,7 +73,7 @@ export default function Listing() {
       </div>
       {shareLinkCopied && (<p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied</p>)}
       <div className="m-4 flex flex-col md:flex-row max-w-2xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white">
-        <div className="w-full h-[200px]">
+        <div className="w-full">
     <p className="text-2xl font-bold mb-3 text-blue-900">
       {listing.name} - ${listing.offer ? listing.discountedPrice.toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
@@ -82,13 +86,13 @@ export default function Listing() {
  <div className="flex justify-start items-center space-x-4 w-[75%]">
   <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">{listing.type="rent" ? "Rent" : "Sale"}</p>
   {listing.offer && (
-    <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">${+(listing.regularPrice) - +(listing.discountedPrice)} dicount</p>
+    <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">${+(listing.regularPrice) - +(listing.discountedPrice)} discount</p>
   )}
  </div>
  <p className="mt-3 mb-3">
   <span className="font-semibold">Description - </span>
  {listing.description}</p>
- <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold">
+ <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold mb-6">
   <li className="flex items-center whitespace-nowrap">
     <FaBed className="text-lg mr-1" />
     {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
@@ -106,8 +110,16 @@ export default function Listing() {
     {listing.furnished ? "Furnished" : "Not Furnished"}
   </li>
  </ul>
+ {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+   <div className="mt-6">
+<button onClick={() => setContactLandlord(true)} className="px-7 py-3 bg-blue-600  text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out ">Contact Landlord</button>
+    </div>
+ )}
+{contactLandlord && (
+  <Contact userRef={listing.userRef} listing={listing} />
+)}
+ </div>
         </div>
-      </div>
     </main>
   )
 }
