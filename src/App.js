271581f4380bc93900653f6043857c5b6337a78c
Nayana Singh <nayanak872@gmail.com>
2022-12-18 22:49:51 +0530

    add home functionality

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<PrivateRoute />} >
         <Route path="/profile" element={<Profile />} /></Route>
         <Route path="/sign-in" element={<SignIn />} />
